60749e9c63dcfe486cf11b842c959c5fed775479
Hibbah Nadeem <hibbah1998@gmail.com>
2019-03-09 17:33:46 +0500

    Tracing Through Directories

---
+++ TracingThroughDirectories.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+echo current directory
+#moving into another directory
+cd directory1
+pwd
+echo files 'in' this current directory1
+#listing all files within that directory including hidden files
+ls -a
+
+#moving into another directory
+cd directory2
+echo current directory
+pwd
+echo files 'in' this current directory2
+
+
+#listing all files within that directory including hidden files
+ls -a
+echo current directory
+#moving into another directory
+cd directory3
+pwd
+echo files 'in' this current directory3
+
+
+#listing all files within that directory including hidden files
+ls -a
